7b8cee44bfc67beba957fd85645c26694fc40f73
Urizao <1135353@atitus.edu.br>
2023-11-21 21:13:43 -0300

    commit 6

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import pygame
 from tkinter import simpledialog
+import pickle
+from arquivo import carregar_imagem
+
 
 pygame.init()
 
 largura = 1280
 altura = 720
-executando = True
 janela = pygame.display.set_mode((largura, altura))
 icone = pygame.image.load('space.png')
-fundo = pygame.image.load("fundo.jpg").convert()  # Carregar a imagem e convertê-la para um formato adequado
+fundo = None
 BRANCO = (255, 255, 255)
 PRETO = (0, 0, 0)
 musica = pygame.mixer.music.load("Musica.mp3")
@@ -20,33 +22,75 @@ pygame.mixer.init()
 pygame.mixer.music.play(-1)
 pygame.display.set_icon(icone)
 
-while executando:
+def desenhar_linhas():
+    for i in range(len(estrelas) - 1):
+        ponto1 = estrelas[i]['pos']
+        ponto2 = estrelas[i + 1]['pos']
+        pygame.draw.line(janela, BRANCO, ponto1, ponto2, 2)
+
+def salvar_marcacoes():
+    with open("marcacoes.pkl", "wb") as arquivo:
+        pickle.dump(estrelas, arquivo)
+    print("Marcações salvas com sucesso!")
+
+def carregar_marcacoes():
+    try:
+        with open("marcacoes.pkl", "rb") as arquivo:
+            estrelas.extend(pickle.load(arquivo))
+        print("Marcações carregadas com sucesso!")
+    except FileNotFoundError:
+        print("Não há marcações salvas.")
+
+def excluir_marcacoes():
+    estrelas.clear()
+    print("Todas as marcações foram excluídas.")
+
+try:
+    fundo = carregar_imagem("fundo.jpg")
+except FileNotFoundError:
+    print("Erro ao carregar a imagem de fundo.")
+
+while True:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
-            executando = False
+            pygame.quit()
+            exit()
         elif evento.type == pygame.MOUSEBUTTONUP:
             pos = pygame.mouse.get_pos()
-            item = simpledialog.askstring("Space", "Nome da Estrela")
-            print(item)
-            if item is None:
-                item = "desconhecido" + str(pos)
-            # estrelas[item] = pos
+            nome = simpledialog.askstring("Space", "Nome da Estrela") or "desconhecido"
+            estrela = {'nome': nome, 'pos': pos}
+            estrelas.append(estrela)
         elif evento.type == pygame.KEYDOWN:
             if evento.key == pygame.K_ESCAPE:
-                executando = False
-        elif evento.type == pygame.MOUSEBUTTONDOWN and evento.button == 1:
-            posicao = pygame.mouse.get_pos()
-            nome = input("Digite o nome da estrela: ")
-            estrelas.append((nome, posicao))
+                pygame.quit()
+                exit()
+            elif evento.key == pygame.K_F10:
+                salvar_marcacoes()
+            elif evento.key == pygame.K_F11:
+                carregar_marcacoes()
+            elif evento.key == pygame.K_F12:
+                excluir_marcacoes()
 
-    janela.blit(fundo, (0, 0))  # Desenhar a imagem de fundo no início do loop
-    for nome, posicao in estrelas:
-        pygame.draw.circle(janela, BRANCO, posicao, 10)
-        texto_surface = fonte.render(nome, True, BRANCO)
-        posicao_texto = texto_surface.get_rect(center=(posicao[0], posicao[1] - 20))
-        janela.blit(texto_surface, posicao_texto)  # Desenhar o texto no centro da estrela
+    janela.fill(PRETO)
+
+    if fundo:
+        janela.blit(fundo, (0, 0))  
+
+    desenhar_linhas()
+
+    for estrela in estrelas:
+        pygame.draw.circle(janela, BRANCO, estrela['pos'], 10)
+        texto = f"{estrela['nome']} ({estrela['pos'][0]}, {estrela['pos'][1]})"
+        texto_surface = fonte.render(texto, True, BRANCO)
+        posicao_texto = texto_surface.get_rect(center=(estrela['pos'][0], estrela['pos'][1] - 20))
+        janela.blit(texto_surface, posicao_texto) 
+
+    texto_salvar = fonte.render("F10 - Salvar marcações", True, BRANCO)
+    texto_carregar = fonte.render("F11 - Carregar marcações", True, BRANCO)
+    texto_excluir = fonte.render("F12 - Excluir marcações", True, BRANCO)
+
+    janela.blit(texto_salvar, (10, 10))
+    janela.blit(texto_carregar, (10, 40))
+    janela.blit(texto_excluir, (10, 70))
 
     pygame.display.update()
-
-pygame.mixer.music.stop()
-pygame.quit()
